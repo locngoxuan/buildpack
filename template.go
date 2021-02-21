d0eec4d598cee9a8d9748581693f4555f0fabe8e
Loc Ngo <xuanloc0511@gmail.com>
2021-02-21 22:25:38 +0700

    remove unneccessary code

--- template.go
+++ template.go
@@ -2,11 +2,6 @@ package main
 
 const (
 	DockerfileOfBuilder = `#generated dockerfile
-FROM alpine:3.13.2 as builder
-MAINTAINER Buildpack <xuanloc0511@gmail.com>
-RUN mkdir -p /working
-ADD . /working
-
 FROM {{.Image}}
 MAINTAINER Buildpack <xuanloc0511@gmail.com>
 RUN mkdir -p /working
